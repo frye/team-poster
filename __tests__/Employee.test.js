0515d87ee7b5245f640b535ec522ce9befaf5ee4
Samuli Jarvinen <frye@github.com>
2022-03-01 00:17:11 -0800

    Basic employee tests, and some code to fix the first one.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,39 @@
 const Employee = require('../lib/Employee');
+const testName = 'Tess Tester';
+const testId = 12354;
+const testEmail = 'tess@test.com';
 
 describe('Employee', () => {
 	describe('Initialization', () => {
-		it.todo('should create an object with name, id and email if provided with valid arguments');
+		it('should create an object with name, id and email if provided with valid arguments', () => {
+			const employee = new Employee(testName, testId, testEmail);
+			let name = employee.name;
+			let id = employee.id;
+			let email = employee.email;
+			expect(name).toEqual(testName);
+			expect(id).toEqual(testId);
+			expect(email).toEqual(testEmail);
+		});
+	});
+	describe('getName()', () => {
+		it('should return the name property', () => {
+			const employee = new Employee(testName, testId, testEmail);
+			let name = employee.getName();
+			expect(name).toEqual(testName);
+		});
+	});
+	describe('getId()', () => {
+		it('should return the id property', () => {
+			const employee = new Employee(testName, testId, testEmail);
+			let id = employee.getId();
+			expect(id).toEqual(testId);
+		});
+	});
+	describe('getEmail()', () => {
+		it('should return the email property', () => {
+			const employee = new Employee(testName, testId, testEmail);
+			let email = employee.getEmail();
+			expect(email).toEqual(testEmail);
+		})
 	});
 });

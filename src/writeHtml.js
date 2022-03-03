7320496ddfecbf6206c1266dbfbc9eb2c8c71e0e
Samuli Jarvinen <frye@github.com>
2022-03-02 19:56:21 -0800

    Added styling

--- src/writeHtml.js
+++ src/writeHtml.js
@@ -14,8 +14,13 @@ const generateHtml = (teamCards) => {
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<title>Team poster</title>
 </head>
-<body>
-	<section id="teamCards" class="d-flex flex-wrap m-3">
+<body class="bg-dark">
+<header>
+<nav class="navbar bg-primary">
+	 <h1 class="text-center w-100">Team Poster</h1>
+</nav>
+</header>
+	<section id="teamCards" class="d-flex flex-wrap m-3 justify-content-center">
 	${teamCards}
 	</section>
 </body>
@@ -27,15 +32,15 @@ const generateHtml = (teamCards) => {
 const generateTeamCards = (teamArray) => {
 	const teamHtmlArr = [];
 	teamArray.forEach((employee) => {
-		teamHtmlArr.push(`<div class="card text-white bg-primary m-2 p-2" style="max-width: 18rem;">
+		teamHtmlArr.push(`<div class="card text-primary bg-dark border-info m-2 p-2 col-4" style="max-width: 18rem;">
 	<div class="card-body">
-	  <h5 class="card-title">${employee.name}</h5>
+	  <h5 class="card-title indigo-800">${employee.name}</h5>
 	  <p class="card-text">${employee.getRole()}</p>
 	</div>
-	<ul class="list-group list-group-flush" id="list">
-	  <li class="list-group-item">ID: ${employee.name}</li>
-	  <li class="list-group-item"><a href="mailto: ${employee.name}">${employee.name}</a></li>
-	  <li class="list-group-item" id="type"></li>
+	<ul class="list-group list-group-flush bg-secondary border-0" id="list">
+	  <li class="list-group-item bg-secondary">ID: ${employee.name}</li>
+	  <li class="list-group-item bg-secondary"><a href="mailto: ${employee.name}" class="link-info">${employee.name}</a></li>
+	  <li class="list-group-item bg-secondary" id="type"></li>
 	</ul>
  </div>
  `);
